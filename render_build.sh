69d0a7276c4d2f1fe1bfa02954823af693cf8bd1
canape360 <fghbg751@yahoo.co.jp>
2026-06-27 19:41:21 +0900

    force delete all staticfiles before collectstatic

--- render_build.sh
+++ render_build.sh
@@ -3,6 +3,6 @@
 set -o errexit
 
 pip install -r requirements.txt
-rm -rf staticfiles
+find staticfiles -type f -delete 2>/dev/null || true
 python manage.py collectstatic --noinput --clear
 python manage.py migrate
