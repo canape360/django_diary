d9a6a5c8e2eb9d8165c2ee6d29418477e6f8bc5d
canape360 <fghbg751@yahoo.co.jp>
2026-06-27 20:08:31 +0900

    restore migrate with fallback

--- render_build.sh
+++ render_build.sh
@@ -5,3 +5,4 @@ set -o errexit
 pip install -r requirements.txt
 find staticfiles -type f -delete 2>/dev/null || true
 python manage.py collectstatic --noinput --clear
+python manage.py migrate --run-syncdb 2>/dev/null || true
